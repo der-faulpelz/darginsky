d71d668687881fd10536e96e9362d8495b588989
der-faulpelz <48916660+der-faulpelz@users.noreply.github.com>
2019-06-06 00:17:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI = os.environ.get('mysql://b2dc52e90f0871:db613689@eu-cdbr-west-02.cleardb.net/heroku_01913328053f6da')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('mysql://b2dc52e90f0871:db613689@eu-cdbr-west-02.cleardb.net/heroku_01913328053f6da')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
