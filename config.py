5ce7917fca1ff8ecdaf659478aad8749d8dd638a
Victoria <victoria_poz@hotmail.com>
2019-06-06 00:52:37 +0300

    Changed database

--- config.py
+++ config.py
@@ -4,8 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('mysql://root:password@localhost/drg3') #or \
-#                              'sqlite:///' + os.path.join(basedir, 'app.db')
-
-#    SQLALCHEMY_DATABASE_URI = "sqlite:///C:\\Users\\vaio\\Downloads\\drg3.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('mysql://b2dc52e90f0871:db613689@eu-cdbr-west-02.cleardb.net/heroku_01913328053f6da') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, select, Table, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("mysql://root:password@localhost/drg3", convert_unicode=True)
+engine = create_engine("mysql://b2dc52e90f0871:db613689@eu-cdbr-west-02.cleardb.net/heroku_01913328053f6da"), convert_unicode=True)
 connection = engine.connect()
 metadata = MetaData()
 
