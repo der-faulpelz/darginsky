00d8986727b832654148b7439b291324cd8137e2
Alex Lebedev <lebedev.alex69@gmail.com>
2019-06-10 19:00:58 +0300

    pipenv dependencies

--- app/config/database.py
+++ app/config/database.py
@@ -6,15 +6,7 @@ engine = create_engine("mysql://b2dc52e90f0871:db613689@eu-cdbr-west-02.cleardb.
 connection = engine.connect()
 metadata = MetaData()
 
-#db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Session = sessionmaker(bind=engine)
 session = Session()
 
 Base = declarative_base()
-#Base.query = db_session.query_property()
-
-#class Query(table_name):
-#    q_tab = Table(table_name, metadata, autoload=True, autoload_with=engine)
-#    sel_tab = select([q_tab])
-#    my_query = sel_tab.where(q_tab.columns.translation_rus="абаз")
-#    connection.execute(my_query).fetchall()
